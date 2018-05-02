95f8b9e19c8dfb6bf433d99fada0a22e06177c9b
Vladimir Grinenko <i@tadatuta.com>
2018-05-02 18:23:12 +0300

    Proper order in platform detection

--- index.js
+++ index.js
@@ -10,13 +10,16 @@ app.use(useragent.express());
 app.use(express.static(path.join(__dirname, 'build')));
 
 app.get('/', function(req, res) {
-    if (req.useragent.isMobile) {
-        res.sendFile(path.resolve(path.join(__dirname), 'build', 'mobile.html'));
-    } else if (req.useragent.isTablet) {
-        res.sendFile(path.resolve(path.join(__dirname), 'build', 'tablet.html'));
+    let platform;
+    if (req.useragent.isTablet) {
+        platform = 'tablet';
+    } else if (req.useragent.isMobile) {
+        platform = 'mobile';
     } else {
-        res.sendFile(path.resolve(path.join(__dirname), 'build', 'desktop.html'));
+        platform = 'desktop';
     }
+
+    res.sendFile(path.resolve(path.join(__dirname), 'build', platform + '.html'));
 });
 
 app.listen(PORT);
